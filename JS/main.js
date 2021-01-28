50b95e7b9b281ff023e9b94e559a82b8943b0d04
vohra1296 <vohraa1270@gmail.com>
2021-01-28 01:24:26 -0600

    Portfolio Initial Commit

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+/*--====Menu  Show ===========--*/
+const showMenu = (toggleId,navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementsById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*--====ACTIVE and REMOVE MENU===========--*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    // Active Link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    // Remove Menu after clicking
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n=> n.addEventListener('click',linkAction))
