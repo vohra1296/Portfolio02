4e6727dbccc279f162871675fe138bf1e8d42f10
vohra1296 <vohraa1270@gmail.com>
2021-01-31 00:41:29 -0600

    Added Pictures and Finished the Structure

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 /*--====Menu  Show ===========--*/
 const showMenu = (toggleId,navId) =>{
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementsById(navId)
+    nav = document.getElementById(navId)
 
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
